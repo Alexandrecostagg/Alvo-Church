b1ab6482eee1166d9c7da6c98d149430fe710da0
Seu Nome <seuemail@exemplo.com>
2026-05-21 12:52:33 -0300

    chore: untrack and ignore apps/web/dist-cloudflare directory to prevent stale mock worker from overriding build

--- apps/web/dist-cloudflare/_worker.js
+++
@@ -1,5 +0,0 @@
-export default {
-  async fetch(request, env) {
-    return new Response("Teste de Inicialização: OK. Se você vê isso, o problema 1101 é no carregamento do handler do Next.js.");
-  }
-};
